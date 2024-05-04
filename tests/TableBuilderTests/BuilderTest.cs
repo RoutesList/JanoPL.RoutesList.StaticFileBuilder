7b7f296a06a83803ac3402b35bcba21a00d0817d
JanoPL <xjanusz4@gmail.com>
2024-05-04 23:20:18 +0200

    [test] add test for table builder

---
+++ tests/TableBuilderTests/BuilderTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using JanoPL.RoutesList.TableBuilder;
+using JetBrains.Annotations;
+
+namespace TableBuilderTests;
+
+[TestSubject(typeof(Builder))]
+public class BuilderTest
+{
+    private readonly Builder _tableBuilder = new();
+
+    [Fact]
+    public void GenerateDataTableTest()
+    {
+        // Arrange
+        PropertyInfo[] properties = typeof(DataHelper.DataHelper).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        
+        IList<DataHelper.DataHelper> list = new List<DataHelper.DataHelper>();
+        for (int i = 1; i <= 3; i++) {
+            string text = "test" + i.ToString();
+            
+            DataHelper.DataHelper helper = new()
+            {
+                Property1 = text,
+                Property2 = text,
+                Property3 = text
+            };
+            
+            list.Add(helper);
+        }
+
+        // Act
+        var results = _tableBuilder.GenerateDataTable(list);
+        
+        // Assert
+        Assert.Equal(3, results.Rows.Count);
+        
+        for (int i = 0; i < properties.Length; i++) {
+            Assert.Equal(properties[i].Name, results.Columns[i].ColumnName);   
+        }
+    }
+}
