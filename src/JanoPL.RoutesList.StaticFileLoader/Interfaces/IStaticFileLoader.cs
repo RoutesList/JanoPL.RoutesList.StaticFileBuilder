ba7f518ee32c00332e52e4112caad872dee3f9ff
JanoPL <xjanusz4@gmail.com>
2024-05-04 23:19:18 +0200

    [feat] add static file loader

---
+++ src/JanoPL.RoutesList.StaticFileLoader/Interfaces/IStaticFileLoader.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace JanoPL.RoutesList.StaticFileLoader.Interfaces;
+
+public interface IStaticFileLoader : IDisposable
+{
+    public Stream LoadFile(string name);
+    public IStaticFileLoader SetAssembly(Assembly? assembly);
+}
---
+++ src/JanoPL.RoutesList.StaticFileLoader/StaticFileLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using JanoPL.RoutesList.StaticFileLoader.Interfaces;
+
+namespace JanoPL.RoutesList.StaticFileLoader;
+
+public class StaticFileLoader : IStaticFileLoader
+{
+    private Assembly? _assembly;
+
+    private Stream? _stream;
+
+    public Stream LoadFile(string name)
+    {
+        _stream = _assembly?.GetManifestResourceStream(name);
+
+        return _stream ?? throw new FileNotFoundException("File cannot be loaded");
+    }
+
+    public IStaticFileLoader SetAssembly(Assembly? assembly)
+    {
+        _assembly = assembly;
+        
+        return this;
+    }
+
+    public void Dispose()
+    {
+        _stream?.Dispose();
+    }
+}
