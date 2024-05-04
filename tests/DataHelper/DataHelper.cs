607cc92df70813b15e6b78cf9b06cf5695dc58ec
JanoPL <xjanusz4@gmail.com>
2024-05-04 23:21:19 +0200

    [test] add DataHelper

---
+++ tests/DataHelper/DataHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace DataHelper;
+
+public class DataHelper
+{
+    public string? Property1 { get; set; }
+    public string? Property2 { get; set; }
+    public string? Property3 { get; set; }
+}
