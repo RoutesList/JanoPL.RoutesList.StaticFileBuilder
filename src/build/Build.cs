93621abfc194bef1ae006ec118abc03c12ebb9a8
JanoPL <xjanusz4@gmail.com>
2024-05-04 23:17:59 +0200

    [build] update nuke build

--- src/build/Build.cs
+++ src/build/Build.cs
@@ -1,44 +1,94 @@
-using System;
 using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using Nuke.Common.Tools.Coverlet;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Serilog;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
-    public static int Main () => Execute<Build>(x => x.Compile);
-
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-        });
+    [Solution] readonly Solution Solution;
 
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-        });
+    [Parameter] readonly AbsolutePath TestResultDirectory = RootDirectory + "/.nuke/Artifacts/Test-Results/";
+    
+    [GitVersion] GitVersion GitVersion;
 
-    Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-        });
+    Target LogInformation =>
+        definition =>
+            definition.Executes(() =>
+            {
+                Log.Information($"Solution path : {Solution}");
+                Log.Information($"Solution directory : {Solution.Directory}");
+                Log.Information($"Configuration : {Configuration}");
+                Log.Information($"TestResultDirectory : {TestResultDirectory}");
+                Log.Information($"Solution path : {Solution}");
+                Log.Information("GitVersion = {Value}", GitVersion.MajorMinorPatch);
+            });
+    
+    Target Preparation =>
+        definition => definition.DependsOn(LogInformation)
+            .Executes(() =>
+            {
+                TestResultDirectory.CreateOrCleanDirectory();
+                Log.Information($"Directory {TestResultDirectory.Name} : create or clean");
+                
+                DotNetTasks.DotNetClean();
+                Log.Information($"Solution has been clean : {Solution.Name}");
+            });
 
+    
+    Target Restore =>
+        definition =>
+            definition.DependsOn(Preparation)
+                .Executes(
+                    () =>
+                    {
+                        DotNetTasks.DotNetRestore();
+                    });
+
+    Target Compile =>
+        definition =>
+            definition
+                .DependsOn(Restore)
+                .Executes(() =>
+                {
+                    DotNetTasks
+                        .DotNetBuild(
+                            build => build.SetProjectFile(Solution)
+                                .SetConfiguration(Configuration)
+                        );
+                });
+
+    Target RunTests =>
+        definition =>
+            definition.DependsOn(Compile)
+                .Executes(() =>
+                {
+                    var testProjects = Solution.AllProjects.Where(s => s.Name.EndsWith("Tests"));
+
+                    DotNetTasks.DotNetTest(
+                        configurator =>
+                            configurator.SetConfiguration(Configuration)
+                                .SetNoBuild(true)
+                                .SetNoRestore(true)
+                                .ResetVerbosity()
+                                .SetResultsDirectory(TestResultDirectory)
+                                .EnableCollectCoverage()
+                                .SetCoverletOutputFormat(CoverletOutputFormat.cobertura)
+                                .SetExcludeByFile("*.Generated.cs")
+                                .EnableUseSourceLink()
+                                .CombineWith(
+                                    testProjects, (settings, project) => settings.SetProjectFile(project)
+                                        .AddLoggers($"trx;LogFileName={project.Name}.trx")
+                                        .SetCoverletOutput(TestResultDirectory + $"{project.Name}.xml")
+                                )
+                    );
+                });
+    public static int Main() => Execute<Build>(x => x.RunTests);
 }
