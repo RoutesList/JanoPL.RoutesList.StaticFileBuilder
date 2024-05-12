aa715df3d4be3b049760b990b027dbc1f9389a79
JanoPL <xjanusz4@gmail.com>
2024-05-12 16:34:44 +0200

    [test] update index compiler

--- tests/IndexCompilerTests/IndexCompilerTest.cs
+++ tests/IndexCompilerTests/IndexCompilerTest.cs
@@ -2,6 +2,8 @@
 using JanoPL.RoutesList.StaticFileBuilder;
 using JanoPL.RoutesList.StaticFileBuilder.Models;
 using JanoPL.RoutesList.StaticFileLoader.Interfaces;
+using JanoPL.RoutesList.TableBuilder;
+using JanoPL.RoutesList.TableBuilder.Interfaces;
 using JetBrains.Annotations;
 using Moq;
 
@@ -17,27 +19,57 @@ public class IndexCompilerTest : IDisposable
     {
         // Arrange
         const string nameTest = "JanoPL.RoutesList.StaticFileBuilder.Resources.StaticFile.index.html";
-
         var staticFileLoaderMock = new Mock<IStaticFileLoader>();
-        staticFileLoaderMock.Setup(loader =>
-            loader.LoadFile(nameTest)).Returns(_stream);
-        
         var sb = new StringBuilder(new StreamReader(_stream).ReadToEnd());
-
         var configDto = new ConfigDto();
+        
+        staticFileLoaderMock.Setup(loader => loader.LoadFile(nameTest)).Returns(_stream);
 
         // Act
         var actualIndexCompiler = new IndexCompiler(sb, configDto);
         var actualSb = actualIndexCompiler.CompileIndex();
 
         // Assert
+        Assert.Equal(sb.ToString(), actualSb.ToString());
+    }
+
+    [Fact]
+    public void IndexCompilerBody()
+    {
+        // Arrange
+        const string nameTest = "JanoPL.RoutesList.StaticFileBuilder.Resources.StaticFile.index.html";
+
+        var staticFileLoaderMock = new Mock<IStaticFileLoader>();
+        // var tableBuilderMock = new Mock<ITableBuilder>();
+        var tableBuilder = new Builder();
+        var sb = new StringBuilder(new StreamReader(_stream).ReadToEnd());
+        var configDto = new ConfigDto();
+        List<DataHelper.TestModel> list = new();
+        var testModel = new DataHelper.TestModel()
+        {
+            Property1 = "test",
+            Property2 = "test",
+            Property3 = "test"
+        };
+        list.Add(testModel);
+        var table = tableBuilder.GenerateDataTable(list);
+
+        staticFileLoaderMock.Setup(loader => loader.LoadFile(nameTest)).Returns(_stream);
         
+        //TODO add HtmlBuilder here to test index compiler
+
+        // Act
+        var actualIndexCompiler = new IndexCompiler(sb, configDto);
+        // actualIndexCompiler.BodyContent = bodyContent;
+        var actualSb = actualIndexCompiler.CompileIndex(false, true);
+
+        // Assert
         Assert.Equal(sb.ToString(), actualSb.ToString());
     }
 
     private static Stream LoadTestFile(string name)
     {
-        var helper = new DataHelper.DataHelper();
+        var helper = new DataHelper.TestModel();
         var stream = helper.GetType().Assembly
             .GetManifestResourceStream(name);
         
