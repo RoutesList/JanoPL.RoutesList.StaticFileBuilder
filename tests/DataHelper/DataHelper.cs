f884446c0452af0099f05860bb5a1a3d5ddd5209
JanoPL <xjanusz4@gmail.com>
2024-05-12 16:32:05 +0200

    [test] update data helper with static file

--- tests/DataHelper/DataHelper.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DataHelper;
-
-public class DataHelper
-{
-    public string? Property1 { get; set; }
-    public string? Property2 { get; set; }
-    public string? Property3 { get; set; }
-}
---
+++ tests/DataHelper/TestModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DataHelper;
+
+public class TestModel
+{
+    public string? Property1 { get; set; }
+    public string? Property2 { get; set; }
+    public string? Property3 { get; set; }
+
+    public IList<TestModel> CreateModelList()
+    {
+        var test = new TestModel()
+        {
+            Property1 = "test 1",
+            Property2 = "test 2",
+            Property3 = "test 3",
+        };
+
+        IList<TestModel> list = new List<TestModel>();
+        list.Add(test);
+
+        return list;
+    }
+}
